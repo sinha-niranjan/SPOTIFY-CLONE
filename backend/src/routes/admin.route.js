78fd392bd216acc1ab4011410f9410fab4fd3d3d
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-12-07 09:16:11 +0530

    create album added

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -16,6 +16,6 @@ router.get("/check", checkAdmin);
 router.post("/songs", createSong);
 router.delete("/songs/:id", deleteSong);
 router.post("/albums", createAlbum);
-router.delete("/albums", deleteAlbum);
+router.delete("/albums/:id", deleteAlbum);
 
 export default router;
